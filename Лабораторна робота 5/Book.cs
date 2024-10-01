27cf2ef8f91600bd331934f010ec4373af5da504
Gen346 <kelzreez@gmail.com>
2024-10-01 11:47:11 +0300

    Add project files.

---
+++ Лабораторна робота 5/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+
+namespace Лабораторна_робота_5
+{
+    internal class Book
+    {
+        private static int _width;
+        private static int _length;
+        private List<string>[,] _listArray;
+        public int Length => _listArray.Length;
+
+        public void SetDimension(int width, int length)
+        {
+            _width = width;
+            _length = length;
+            _listArray = new List<string>[_width, _length];
+
+            for(int i = 0; i < _width; i++)
+            {
+                for(int j = 0; j < _length; j++)
+                {
+                    _listArray[i, j] = new List<string>();
+                }
+            }
+        }
+
+        public List<string> this[int index1, int index2]
+        {
+            get => _listArray[index1, index2];
+            set => _listArray[index1, index2] = value;
+        }
+
+        public void PrintSize()
+        {
+            Console.WriteLine($"Width of array: {_width}");
+            Console.WriteLine($"Length of array: {_length}");
+        }
+    }
+}
---
+++ Лабораторна робота 5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Лабораторна_робота_5;
+
+Book book = new Book();
+book.SetDimension(5, 5);
+book[0, 0].Add("HelloBook");
+book[0, 1].Add("SecondBook");
+
+for(int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        for(int k = 0; k < book[i, j].Count; k++)
+        {
+        Console.WriteLine($"{book[i, j][k]}"); 
+        }
+    }
+}
